5d6a16b6489875d1168bc9b1e1d0a6659af80a1e
Benaya Chessa <126884528+benayachessa@users.noreply.github.com>
2026-02-08 21:28:05 +0700

    optimasi seo dan menghilangkan menu produk

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,21 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import vercel from "@astrojs/vercel/static";
 
+import vitePwa from "@vite-pwa/astro";
+
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
   output: "static",
+
   adapter: vercel({
     imageService: true,
   }),
+
+  integrations: [vitePwa(), sitemap()],
 });
